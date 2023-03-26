7eb24598945d7e19ede1c8dd4c6d83dd3414124e
Pablo Sivina <pablosivina@gmail.com>
2023-03-25 22:14:51 -0300

    mejoro navbar

--- src/Component/NavBar/NavBar.js
+++ src/Component/NavBar/NavBar.js
@@ -7,28 +7,26 @@ const NavBar = ()=> {
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
     <a className="navbar-brand" href="#"><LogoBeatrice /></a>
+    
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
+    
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link" href="#">Facial</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
+          <a className="nav-link" href="#">Corporal</a>
         </li>
       <li className="nav-item">
-         <a className="nav-link disabled">Disabled</a>
+         <a className="nav-link disabled">Bienestar</a>
         </li>
       </ul>
     </div>
+    <CartWidget />
   </div>
-  <CartWidget />
-  
 </nav>
     )
 }
