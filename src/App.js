11ca46614487226802235cca069e9ead7bdb0aba
Pablo Sivina <pablosivina@gmail.com>
2023-04-24 00:01:22 -0300

    Ent Final agrego CartView

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './Component/NavBar/NavBar';
 import ItemListContainer from './Component/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './Component/ItemDetailContainer/ItemDetailContainer';
-import { CartPovider } from './Component/Context/CartContext';
+import Cart from './Component/Cart/Cart';
+import { CartProvider } from './Component/Context/CartContext';
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <CartPovider>
+        <CartProvider>
           <NavBar />
           <div className="container-fluid containerCard">
             <div className='row'>
@@ -21,13 +22,12 @@ function App() {
                 <Route path='/' element={<ItemListContainer />} />
                 <Route path='/category/:categoryId' element={<ItemListContainer />} />
                 <Route path='/item/:itemId' element={<ItemDetailContainer />} />
+                <Route path='/cart' element={<Cart />} />
                 <Route path='*' element={<h1>404 NOT FOUND</h1>} />
-
-
               </Routes>
             </div>
           </div>
-        </CartPovider>
+        </CartProvider>
       </BrowserRouter>
     </div>
   );
--- src/Component/CartWidget/CartWidjet.js
+++ src/Component/CartWidget/CartWidjet.js
@@ -1,13 +1,17 @@
+import { useContext } from 'react';
 import Carrito from './assets/Carrito.png'
 import './Carrito.css';
+import { CartContext } from '../Context/CartContext';
+import { Link } from 'react-router-dom';
 
-const CartWidget =() =>{
-    return(
-        <div className = 'carrito'>
-            <div>0</div>
-            <img className ='carrito-img'src = {Carrito} alt="carrito-widget"/>
-            
-        </div>
+const CartWidget = () => {
+    const { totalQuantity } = useContext(CartContext)
+
+    return (
+        <Link to='/cart' className='carrito' style={{ display: totalQuantity > 0 ? 'block' : 'none' }}>
+            <img className='carrito-img' src={Carrito} alt="carrito-widget" />
+            {totalQuantity}
+        </Link>
     )
 }
 
