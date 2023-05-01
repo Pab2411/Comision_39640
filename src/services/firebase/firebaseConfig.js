4615831ed1f843e8c9907b54b93166d6a9d73076
Pab2411 <pablosivina@gmail.com>
2023-05-01 20:48:23 -0300

    Ent-Final coloco .env

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCRIgrQOv_Lz2z12IwXI-mTrxbUDfoiOyI",
-    authDomain: "beatrice-e69a6.firebaseapp.com",
-    projectId: "beatrice-e69a6",
-    storageBucket: "beatrice-e69a6.appspot.com",
-    messagingSenderId: "349073045423",
-    appId: "1:349073045423:web:87f1da0006a5cd2f39a15a"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
